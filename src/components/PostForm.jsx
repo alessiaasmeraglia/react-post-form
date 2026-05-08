0521a4c35bb790b4632eb61966cb9e15aaf1d0df
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-08 17:34:14 +0200

    Aggiunta handleChange generica

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -3,10 +3,27 @@ import { useState } from "react";
 const API_URL = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
 
 function PostForm() {
-    const [formData, setFormData] = useState ({
+    const [formData, setFormData] = useState({
         author: "",
         title: "",
         body: "",
         public: false,
     });
+
+    const handleChange = (event) => {
+        const target = event.target;
+        const tagType = target.type;
+        const name = target.name;
+        const value = target.value;
+        const checked = target.checked;
+
+        const valueToUpdate = tagType === "checkbox" ? checked : value;
+
+        setFormData({
+            ...formData,
+            [name]: valueToUpdate,
+        });
+    };
 }
+
+export default PostForm;
